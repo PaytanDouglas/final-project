527bd80de8dd57ea60dcf9bb34e9a91fb84ae927
PaytanDouglas <douglap@clarkson.edu>
2019-12-09 17:55:26 -0500

    (hopefully) finished the menu page, added modals

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
- AOS.init({
+//Modal
+$(".button").on("click", function() {
+  var modal = $(this).data("modal");
+  $(modal).show();
+});
+$(".modal").on("click", function(e) {
+  var className = e.target.className;
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide();
+  }
+});
+
+//The rest
+AOS.init({
  	duration: 800,
  	easing: 'slide',
  	once: false
